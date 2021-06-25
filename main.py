773de07a58a3f36264d88c90eeb76b99f3019964
Krishna Epili <k.epili@bd.com>
2021-06-25 21:56:58 +0400

    first commit of our application

---
+++ main.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/t',methods=['GET','POST'])
+def index():
+    return'<h1>This is flask application</h1>'
+if __name__ == '__main__':
+    app.run()
